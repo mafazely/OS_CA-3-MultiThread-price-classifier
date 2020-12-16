6c9190bdb9f9cf057ffa37571d7c5f0920fa8762
mafazely <aminfazeli00@gmail.com>
2020-12-16 15:24:07 +0330

    add header files and structs

--- parallel/main.cpp
+++ parallel/main.cpp
@@ -1,1 +1,26 @@
 // multi thread implementation
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <pthread.h>
+#include <limits.h>
+
+#define NUMBER_OF_THREADS 4
+#define MAXVALUE 1000000000
+
+using namespace std;
+
+struct csvFile
+{
+    vector<vector<double>> numericMatrix;
+    vector<string> headerFile;
+};
+typedef struct csvFile csv;
+typedef map<string, vector<double>> minMaxStruct;
+
+struct threadData
+{
+    csv dataset;
+    double numOfCorrect;
+    double total = dataset.numericMatrix.size();
+};
+typedef struct threadData thData;
